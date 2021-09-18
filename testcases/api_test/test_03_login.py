53b34a8964912d43374883cc46f961cd121e0dfa
zhangxu36925800 <zhangxu36925800@163.com>
2021-09-18 20:34:28 +0800

    testdemo

--- testcases/api_test/test_03_login.py
+++ testcases/api_test/test_03_login.py
@@ -12,12 +12,13 @@ def step_1(username):
 @allure.epic("针对单个接口的测试")
 @allure.feature("用户登录模块")
 class TestUserLogin():
-    @allure.story("用例--登录用户")
+    @allure.tag("接口请求地址:")
+    @allure.title("用例--登录用户")
     @allure.description("该用例是针对获取用户登录接口的测试")
-    @allure.issue("http://localhost:8009/api/auth/login", name="点击，跳转到对应BUG的链接地址")
-    @allure.testcase("http://localhost:8009/api/auth/login", name="点击，跳转到对应用例的链接地址")
-    @allure.title("测试数据：【 {username}，{password}，{except_result}，{except_code}，{except_msg}】")
-    @pytest.mark.single
+    # @allure.issue("http://localhost:8009/api/auth/login", name="点击，跳转到对应BUG的链接地址")
+    # @allure.testcase("http://localhost:8009/api/auth/login", name="点击，跳转到对应用例的链接地址")
+    # @allure.title("测试数据：【 {username}，{passwordz}，{except_result}，{except_code}，{except_msg}】")
+    # @pytest.mark.single
     @pytest.mark.parametrize("username, password, except_result, except_code, except_msg",
                              api_data["test_login_user"])
     def test_login_user(self, username, password, except_result, except_code, except_msg):
