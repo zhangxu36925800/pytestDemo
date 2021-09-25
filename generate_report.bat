5e5fb46ee8e597f5d965f671739e7eb53132aef2
zhangxu36925800 <zhangxu36925800@163.com>
2021-09-25 14:11:56 +0800

    testdemo

--- generate_report.bat
+++ generate_report.bat
@@ -4,11 +4,3 @@ call D:
 call cd D:\Document\pycharmworkspace\pytestDemo\testcases\api_test
 call allure generate ./report/ -o D:\Document\pythonreport --clean
 call cd D:\Document&&allure open pythonreport
-=======
-@echo off
-call E:
-call cd E:\document\pycharm-workspace\pytestdemo\testcases\api_test
-call allure generate ./report/ -o E:\document\pythonreport --clean
-call cd E:\document&&allure open pythonreport
->>>>>>> origin/master
-pause
